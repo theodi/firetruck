612f009a88eb3d6199ccbc763293d89aa38edcbd
julie freeman <1784280+joz-f@users.noreply.github.com>
2020-02-03 12:31:05 +0000

    Add files via upload

---
+++ data/country_codes.js
@@ -0,0 +1,102 @@
+const country_codes = {
+  "German": {
+    "code": "DE",
+    "capital": "Berlin"
+  },
+  "Latvian": {
+    "code": "LV",
+    "capital": "Riga"
+  },
+  "Hungarian": {
+    "code": "HU",
+    "capital": "Budapest"
+  },
+  "Welsh": {
+    "code": "GB-WLS",
+    "capital": "Cardiff"
+  },
+  "Polish": {
+    "code": "PL",
+    "capital": "Warsaw"
+  },
+  "Danish": {
+    "code": "DK",
+    "capital": "Copenhagen"
+  },
+  "Scottish Gaelic": {
+    "code": "GB-SCT",
+    "capital": "Edinburgh"
+  },
+  "Slovene": {
+    "code": "SI",
+    "capital": "Ljublijana"
+  },
+  "Spanish (Castilian)": {
+    "code": "ES",
+    "capital": "Madrid"
+  },
+  "Finnish": {
+    "code": "FI",
+    "capital": "Helsinki"
+  },
+  "Greek": {
+    "code": "GR",
+    "capital": "Athens"
+  },
+  "Swedish": {
+    "code": "SE",
+    "capital": "Stockholm"
+  },
+  "Czech": {
+    "code": "CZ",
+    "capital": "Prague"
+  },
+  "Croatian": {
+    "code": "HR",
+    "capital": "Zagreb"
+  },
+  "Estonian": {
+    "code": "EE",
+    "capital": "Tallinn"
+  },
+  "Maltese": {
+    "code": "MT",
+    "capital": "Valletta"
+  },
+  "Bulgarian": {
+    "code": "BG",
+    "capital": "Sofia"
+  },
+  "Lithuanian": {
+    "code": "LT",
+    "capital": "Vilnius"
+  },
+  "Romanian": {
+    "code": "RO",
+    "capital": "Bucharest"
+  },
+  "Irish": {
+    "code": "IE",
+    "capital": "Dublin"
+  },
+  "Portuguese": {
+    "code": "PT",
+    "capital": "Lisbon"
+  },
+  "Dutch": {
+    "code": "NL",
+    "capital": "Amsterdam"
+  },
+  "Italian": {
+    "code": "IT",
+    "capital": "Rome"
+  },
+  "Slovak": {
+    "code": "SK",
+    "capital": "Bratislava"
+  },
+  "French": {
+    "code": "FR",
+    "capital": "Paris"
+  }
+};
---
+++ data/sequence.js
@@ -0,0 +1,55 @@
+const language_sequence = [
+    "German"            ,
+    "Latvia"            ,
+    "Hungarian"         ,
+    "Welsh"             ,
+    "Polish"            ,
+    "Danish"            ,
+    "Scottish"          , /* Gaelic */
+    "Slovene"           ,
+    "Spanish"           , /* (Casillian) */
+    "Finnish"           ,
+    "Greek"             ,
+    "Swedish"           ,
+    "Czech"             ,
+    "Croatian"          ,
+    "Estonian"          ,
+    "Maltese"           ,
+    "Bulgarian"         ,
+    "Lithuanian"        ,
+    "Romanian"          ,
+    "Irish"             ,
+    "Portuguese"        ,
+    "Dutch"             ,
+    "Italian"           ,
+    "Slovak"            ,
+    "French"
+];
+
+/*
+BGR,Bulgaria,Public domain
+CZE,Czech Republic,Public domain
+DEU,Germany,Public domain
+DNK,Denmark,Public domain
+ESP,Spain,Public domain
+EST,Estonia,Public domain
+FIN,Finland,Public domain
+FRA,France,Public domain
+GBR,United Kingdom,Public domain
+GRC,Greece,Public domain
+HRV,Croatia,Public domain
+HUN,Hungary,Public domain
+IRL,Ireland,Public domain
+ITA,Italy,Public domain
+LTU,Lithuania,Public domain
+LVA,Latvia,Public domain
+MLT,Malta,Public domain
+NLD,Netherlands,Public domain
+POL,Poland,Public domain
+PRT,Portugal,Public domain
+ROU,Romania,Public domain
+SVK,Slovakia,Public domain
+SVN,Slovenia,Public domain
+SWE,Sweden,Public domain
+
+ */
